a344a2dfd3049ccdbb4a3219fc90897fc1442f3f
mohizuki <upalphb4@gmail.com>
2023-09-24 16:20:53 +0800

    [FIX] changing card

--- script.js
+++ script.js
@@ -5,24 +5,24 @@ const content = document.querySelector('.content');
 toggleSidebarButton.addEventListener('click', () => {
     sidebar.classList.toggle('active');
     if (sidebar.classList.contains('active')) {
-        sidebar.style.width = '250px';
-    } else {
         sidebar.style.width = '0';
+    } else {
+        sidebar.style.width = '250px';
     }
 });
 
-// Add this code to handle smaller screens (e.g., phones)
-const mediaQuery = window.matchMedia('(max-width: 720px)');
+// // Add this code to handle smaller screens (e.g., phones)
+// const mediaQuery = window.matchMedia('(max-width: 720px)');
 
-function handleMobileView(event) {
-    if (event.matches) {
-        sidebar.classList.remove('active');
-        sidebar.style.width = '0';
-    } else {
-        // Reset sidebar for larger screens
-        sidebar.style.width = '250px'; // Adjust the width as needed
-    }
-}
+// function handleMobileView(event) {
+//     if (event.matches) {
+//         sidebar.classList.remove('active');
+//         sidebar.style.width = '0';
+//     } else {
+//         // Reset sidebar for larger screens
+//         sidebar.style.width = '250px'; // Adjust the width as needed
+//     }
+// }
 
-mediaQuery.addListener(handleMobileView);
-handleMobileView(mediaQuery);
+// mediaQuery.addListener(handleMobileView);
+// handleMobileView(mediaQuery);
